5082d9289856ee56271becb84d56d15f5fc1e418
Sacred744 <gejianqiang555@gmail.com>
2024-10-10 15:46:30 +0800

    up添加项目文件。

---
+++ Form2.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace 记事本
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonRename = new System.Windows.Forms.Button();
+            this.buttonChecked = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 21;
+            this.listBox1.Location = new System.Drawing.Point(66, 21);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(358, 592);
+            this.listBox1.TabIndex = 0;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(473, 21);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(143, 47);
+            this.buttonAdd.TabIndex = 1;
+            this.buttonAdd.Text = "添加";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(473, 94);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(143, 47);
+            this.buttonDelete.TabIndex = 2;
+            this.buttonDelete.Text = "删除";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonRename
+            // 
+            this.buttonRename.Location = new System.Drawing.Point(473, 169);
+            this.buttonRename.Name = "buttonRename";
+            this.buttonRename.Size = new System.Drawing.Size(143, 47);
+            this.buttonRename.TabIndex = 3;
+            this.buttonRename.Text = "重命名";
+            this.buttonRename.UseVisualStyleBackColor = true;
+            this.buttonRename.Click += new System.EventHandler(this.buttonRename_Click);
+            // 
+            // buttonChecked
+            // 
+            this.buttonChecked.Location = new System.Drawing.Point(473, 256);
+            this.buttonChecked.Name = "buttonChecked";
+            this.buttonChecked.Size = new System.Drawing.Size(143, 47);
+            this.buttonChecked.TabIndex = 4;
+            this.buttonChecked.Text = "确认修改";
+            this.buttonChecked.UseVisualStyleBackColor = true;
+            this.buttonChecked.Click += new System.EventHandler(this.buttonChecked_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(473, 579);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(143, 47);
+            this.buttonCancel.TabIndex = 5;
+            this.buttonCancel.Text = "退出修改";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 652);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonChecked);
+            this.Controls.Add(this.buttonRename);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form2";
+            this.Text = "窗体管理";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonRename;
+        private System.Windows.Forms.Button buttonChecked;
+        private System.Windows.Forms.Button buttonCancel;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,188 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace 记事本
+{
+    public partial class Form2 : Form
+    {
+        
+        private List<String> TabNames = Properties.Settings.Default["TabNames"].ToString().Split(',').ToList();
+        private JArray TabData = JArray.Parse(Properties.Settings.Default["TabData"].ToString());
+        private Form InputForm = new Form();
+        private string userInput;
+        private TextBox textBox = new TextBox();
+        private Button button = new Button();
+        public Form2()
+        {
+            this.Icon = Properties.Resources.book;
+            InitializeComponent();
+            InitListbox();
+            InitInputForm();
+        }
+        public void InitListbox()
+        {
+            listBox1.Items.Clear();
+            foreach (String name in TabNames)
+            {
+                listBox1.Items.Add(name.ToString());
+            }
+
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("确定要执行删除操作吗？", "警告", MessageBoxButtons.OKCancel);
+            if (result == DialogResult.OK)
+            {
+                TabNames.Remove(listBox1.SelectedItem.ToString());
+                TabData.Remove(TabData[listBox1.SelectedIndex]);
+                InitListbox();
+            }
+            else if (result == DialogResult.Cancel)
+            {
+                
+            }
+
+        }
+
+        private void buttonChecked_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default["TabNames"] = string.Join(",", TabNames);
+            Properties.Settings.Default["TabData"] = TabData.ToString();
+            Properties.Settings.Default.Save();
+            this.Close();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            InputForm.ShowDialog();
+            if (!string.IsNullOrEmpty(userInput)) {
+                TabNames.Add(userInput);
+                TabData.Add("");
+                InitListbox();
+            }
+            
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+
+        private void InitInputForm() {
+
+            InputForm.Icon = Properties.Resources.book;
+            InputForm.Size=new Size(300, 150);
+            // 创建一个文本框控件
+            
+            textBox.Location = new System.Drawing.Point(10, 10);
+            textBox.Width = 200;
+            InputForm.Controls.Add(textBox);
+
+            // 创建一个按钮控件
+            
+            button.Text = "确定";
+            button.Location = new System.Drawing.Point(120, 40);
+            InputForm.Controls.Add(button);
+
+            // 创建一个按钮控件
+            Button buttonCancel = new Button();
+            buttonCancel.Text = "取消";
+            buttonCancel.Location = new System.Drawing.Point(10, 40);
+            InputForm.Controls.Add(buttonCancel);
+
+            textBox.KeyDown += InputForm_KeyDown;
+
+            // 按钮点击事件处理程序
+            button.Click += button_Click;
+
+            InputForm.FormClosing += (sender, e) =>
+            {
+                textBox.Text = "";
+            };
+
+            buttonCancel.Click += (sender, e) =>
+            {
+                InputForm.Close();
+                userInput = null;
+
+            };
+            InputForm.KeyDown += (sender, e) =>
+            {
+                if (e.KeyCode == Keys.Escape)
+                {
+                    InputForm.Close();
+                    userInput = null;
+                }
+            };
+
+        }
+
+        void button_Click(object sender,EventArgs e)
+        {
+            userInput = textBox.Text;
+            if (!string.IsNullOrEmpty(userInput))
+            {
+                InputForm.Close();
+            }
+            else
+            {
+                MessageBox.Show("不得为空");
+            }
+        }
+
+
+        void InputForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button_Click(sender, e);
+            }
+            else if (e.KeyCode == Keys.Escape) {
+
+                InputForm.Close();
+                userInput = null;
+
+            }
+        }
+
+
+
+        public void ReturnInput(object sender, FormClosedEventArgs e) 
+        {
+            if (sender is Form)
+            {
+                InputForm.FormClosed += ReturnInput;
+            }
+        }
+
+        private void buttonRename_Click(object sender, EventArgs e)
+        {
+            if (TabNames[listBox1.SelectedIndex] != null)
+            {
+                InputForm.ShowDialog();
+                if (!string.IsNullOrEmpty(userInput))
+                {
+                    TabNames[listBox1.SelectedIndex] = userInput;
+                    InitListbox();
+                }
+            }
+            else {
+
+                MessageBox.Show("先选择需要修改的项");
+            }
+            
+        }
+    }
+}
